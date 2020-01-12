d221990fb753192c7f2270e3cf86af1a16f669fa
Thuc Le <ledongthuc@gmail.com>
2020-01-12 20:19:56 +0100

    Core lib subtraction

--- subtraction.go
+++ subtraction.go
@@ -1,25 +1,27 @@
 package mathgen
 
 import (
+	"errors"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 )
 
-// Substraction Result of addition operator
-type SubstractionResult struct {
+// SubtractionResult of addition operator
+type SubtractionResult struct {
 	Minuend     int64
 	Subtrahends []int64
 	Difference  int64
 }
 
-// String of SubstractionResult will present struct data with question answer format a - b - c - d ... = difference
-func (s SubstractionResult) String() string {
+// String of SubtractionResult will present struct data with question answer format a - b - c - d ... = difference
+func (s SubtractionResult) String() string {
 	return s.StringQuestion() + strconv.FormatInt(s.Difference, 10)
 }
 
-// StringQuestion of SubstractionResult will present struct data with question format a - b - c - d ... =
-func (s SubstractionResult) StringQuestion() string {
+// StringQuestion of SubtractionResult will present struct data with question format a - b - c - d ... =
+func (s SubtractionResult) StringQuestion() string {
 	if len(s.Subtrahends) == 0 {
 		return fmt.Sprintf("%d = ", s.Minuend)
 	}
@@ -34,4 +36,50 @@ func (s SubstractionResult) StringQuestion() string {
 	}
 	b.WriteString(" = ")
 	return b.String()
+}
+
+// SubtractIntegers generate a operator with subtraction of 1 minuend and 1 subtrahend.
+// Parameter maxMinuend defines maxsimum of minuend, it should be greater than 0.
+// If maxMinuend = 1, addends always is 1 - 1
+func SubtractIntegers(maxMinuend int64) (SubtractionResult, error) {
+	return subtractIntegerN(getRand(), 2, maxMinuend)
+}
+
+// SubtractIntegerN generate a operator with subtraction.
+// Parameter numberOfSubtrahends defines subtrahends in question, it should be greater than 0.
+// Parameter maxMinuend defines maxsimum of minuend result, it should be greater than 0.
+// If maxMinuend = 1, addends always is 1 - 1 - 0 ....
+func SubtractIntegerN(numberOfSubtrahends int, maxMinuend int64) (SubtractionResult, error) {
+	return subtractIntegerN(getRand(), numberOfSubtrahends, maxMinuend)
+}
+
+// subtractIntegerN generate a operator with subtraction.
+// r is source of random numbers
+// Parameter numberOfSubtrahends defines subtrahends in question, it should be greater than 0.
+// Parameter maxMinuend defines maxsimum of minuend result, it should be greater than 0.
+// If maxMinuend = 1, addends always is 1 - 1 - 0 ....
+func subtractIntegerN(r *rand.Rand, numberOfSubtrahends int, maxMinuend int64) (SubtractionResult, error) {
+	if numberOfSubtrahends <= 0 {
+		return SubtractionResult{}, errors.New("number of subtrahend should be greater than 0")
+	}
+	if maxMinuend <= 0 {
+		return SubtractionResult{}, errors.New("max minuend should be greater than 0")
+	}
+	if maxMinuend <= int64(numberOfSubtrahends) {
+		subtrahends := make([]int64, maxMinuend, maxMinuend)
+		for i := int64(0); i < maxMinuend; i++ {
+			subtrahends[i] = 1
+		}
+		return SubtractionResult{Minuend: maxMinuend, Subtrahends: subtrahends, Difference: 0}, nil
+	}
+
+	additionResult, err := addIntegerN(r, numberOfSubtrahends+1, maxMinuend)
+	if err != nil {
+		return SubtractionResult{}, err
+	}
+	return SubtractionResult{
+		Minuend:     additionResult.Sum,
+		Subtrahends: additionResult.Addends[:len(additionResult.Addends)-1],
+		Difference:  additionResult.Addends[len(additionResult.Addends)-1],
+	}, nil
 }
--- subtraction_test.go
+++ subtraction_test.go
@@ -1,10 +1,12 @@
 package mathgen
 
 import (
+	"math/rand"
+	"reflect"
 	"testing"
 )
 
-func TestSubstractionResult_StringQuestion(t *testing.T) {
+func TestSubtractionResult_StringQuestion(t *testing.T) {
 	type fields struct {
 		Minuend     int64
 		Subtrahends []int64
@@ -45,19 +47,19 @@ func TestSubstractionResult_StringQuestion(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := SubstractionResult{
+			s := SubtractionResult{
 				Minuend:     tt.fields.Minuend,
 				Subtrahends: tt.fields.Subtrahends,
 				Difference:  tt.fields.Difference,
 			}
 			if got := s.StringQuestion(); got != tt.want {
-				t.Errorf("SubstractionResult.StringQuestion() = %v, want %v", got, tt.want)
+				t.Errorf("SubtractionResult.StringQuestion() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestSubstractionResult_String(t *testing.T) {
+func TestSubtractionResult_String(t *testing.T) {
 	type fields struct {
 		Minuend     int64
 		Subtrahends []int64
@@ -98,13 +100,178 @@ func TestSubstractionResult_String(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := SubstractionResult{
+			s := SubtractionResult{
 				Minuend:     tt.fields.Minuend,
 				Subtrahends: tt.fields.Subtrahends,
 				Difference:  tt.fields.Difference,
 			}
 			if got := s.String(); got != tt.want {
-				t.Errorf("SubstractionResult.String() = %v, want %v", got, tt.want)
+				t.Errorf("SubtractionResult.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_subtractIntegerN(t *testing.T) {
+	type args struct {
+		r                   *rand.Rand
+		numberOfSubtrahends int
+		maxMinuend          int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    SubtractionResult
+		wantErr bool
+	}{
+		{
+			name: "number of subtrahends is 0",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 0,
+				maxMinuend:          10,
+			},
+			want:    SubtractionResult{},
+			wantErr: true,
+		},
+		{
+			name: "max minuend is 0",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 1,
+				maxMinuend:          0,
+			},
+			want:    SubtractionResult{},
+			wantErr: true,
+		},
+		{
+			name: "max minuend equals number of subtrahends",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 5,
+				maxMinuend:          5,
+			},
+			want: SubtractionResult{
+				Minuend:     5,
+				Subtrahends: []int64{1, 1, 1, 1, 1},
+				Difference:  0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "max minuend is less than number of subtrahends",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 5,
+				maxMinuend:          3,
+			},
+			want: SubtractionResult{
+				Minuend:     3,
+				Subtrahends: []int64{1, 1, 1},
+				Difference:  0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "max minuend 5 - 1st",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 1,
+				maxMinuend:          5,
+			},
+			want: SubtractionResult{
+				Minuend:     3,
+				Subtrahends: []int64{2},
+				Difference:  1,
+			},
+			wantErr: false,
+		},
+		{
+			name: "max minuend 5 - 2st",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 1,
+				maxMinuend:          5,
+			},
+			want: SubtractionResult{
+				Minuend:     3,
+				Subtrahends: []int64{1},
+				Difference:  2,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Successful - large number 1st",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 3,
+				maxMinuend:          1000,
+			},
+			want: SubtractionResult{
+				Minuend:     844,
+				Subtrahends: []int64{270, 169, 169},
+				Difference:  236,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Successful - large number 2nd",
+			args: args{
+				r:                   rand.New(rand.NewSource(0)),
+				numberOfSubtrahends: 3,
+				maxMinuend:          1000,
+			},
+			want: SubtractionResult{
+				Minuend:     844,
+				Subtrahends: []int64{106, 281, 141},
+				Difference:  316,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := subtractIntegerN(tt.args.r, tt.args.numberOfSubtrahends, tt.args.maxMinuend)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("subtractIntegerN() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subtractIntegerN() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_subtractIntegerMass(t *testing.T) {
+	type args struct {
+		r                   *rand.Rand
+		numberOfSubtrahends int
+		maxMinuend          int64
+	}
+	tt := struct {
+		name string
+		args args
+	}{
+		name: "Successful",
+		args: args{
+			r:                   rand.New(rand.NewSource(0)),
+			numberOfSubtrahends: 100,
+			maxMinuend:          1000,
+		},
+	}
+	for i := 0; i < 10000; i++ {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := subtractIntegerN(tt.args.r, tt.args.numberOfSubtrahends, tt.args.maxMinuend)
+			if err != nil {
+				t.Errorf("subtractIntegerN() error = %v", err)
+			}
+			actuallyDifference := got.Minuend
+			for _, subtrahend := range got.Subtrahends {
+				actuallyDifference -= subtrahend
+			}
+			if actuallyDifference != got.Difference {
+				t.Errorf("subtractIntegerN() at index %v = %v, want %v", i, actuallyDifference, got.Difference)
 			}
 		})
 	}
